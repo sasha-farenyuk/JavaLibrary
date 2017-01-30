3f941b28c48ae78290d71519a6ee02e887e5d46a
Sasha Farenyuk <sasha.farenyuk@gmail.com>
2017-01-30 17:45:09 -0500

    Initial commit

---
+++ src/com/farenyuk/sasha/JavaLibrary/Main.java
@@ -0,0 +1,29 @@
+package com.farenyuk.sasha.JavaLibrary;
+public class Main {
+    public static void main(String[] args) {
+        Pi();
+        compInt();
+    }
+    public static void Pi(){
+        double Pi = 0;
+        int j = 1;
+        for(int i=0; i<=10000; i++){
+            Pi+=Math.pow((-1), i)/j;
+            j+=2;
+        }
+        System.out.println(4*Pi);
+    }
+    public static void compInt(){
+        double P = 10000.0;
+        double I = 0.0;
+        double r = 0.05;
+        int y= 0;
+        double SV = 2.0*P;
+        while(P<SV){
+            y+=1;
+            I=P*r;
+            P+=I;
+            System.out.printf("year: %d p: %.2f\n", y, P);
+        }
+    }
+}
