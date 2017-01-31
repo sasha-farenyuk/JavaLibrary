7d937599efde28214f299f87adaf5a195211c267
Sasha Farenyuk <sasha.farenyuk@gmail.com>
2017-01-30 19:10:46 -0500

    i made some comments

--- src/com/farenyuk/sasha/JavaLibrary/Main.java
+++ src/com/farenyuk/sasha/JavaLibrary/Main.java
@@ -1,8 +1,10 @@
 package com.farenyuk.sasha.JavaLibrary;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Pi();
-        compInt();
+        //Pi();
+        //compInt();
+        HeronsTri();
     }
     public static void Pi(){
         //Calculating the precise number of PI
@@ -17,7 +19,7 @@ public class Main {
     public static void compInt(){
         // Finding how many years until the final value is twice more then original value
         double P = 10000.0;
-        double I = 0.0;
+        double I;
         double r = 0.05;
         int y= 0;
         double SV = 2.0*P;
@@ -27,5 +29,15 @@ public class Main {
             P+=I;
             System.out.printf("year: %d p: %.2f\n", y, P);
         }
+    }
+    public static void HeronsTri(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("You have a triangle, Enter all three sides to find area.");
+        double a = scan.nextDouble();
+        double b = scan.nextDouble();
+        double c = scan.nextDouble();
+        double P = a+b+c;
+        double HT = Math.sqrt(P/2 * (P/2 - a)*(P/2 - b)*(P/2 -c));
+        System.out.println("The Area is " + HT);
     }
 }
